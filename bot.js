04b32111ca686bbe8c9dff409bcefe6e958174b8
RamiqK <43069933+RamiqK@users.noreply.github.com>
2018-09-07 17:20:28 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,16 @@
+﻿const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('message', message => {
+    if(message.content.toLowerCase() === 'sen kafayı sıyırmışsın'){
+      message.reply(':`)')
+    }
+})kafa 
+
+
+bot.on('ready', () => {
+     bot.user.setActivity('Acılarla');
+     console.log('Bot Hazır');
+});    // bot hazır ve bot oyunu
+
+bot.login(process.env.BOT_TOKEN)
